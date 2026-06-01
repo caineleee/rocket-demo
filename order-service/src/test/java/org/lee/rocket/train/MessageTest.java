d5fe7f1d8cc5155b28a1736551f175463bfc2fd5
caineleee <dearfuhua@outlook.com>
2026-06-01 22:32:27 +0800

    refactor(project): 重构项目结构引入微服务架构
    - 将原有的 producer 和 consumer 模块重命名为 order-service 和 pay-service
    - 更新 pom.xml 中的模块配置，移除旧模块并添加新的服务模块
    - 为 order-service 添加 Docker Compose 集成支持
    - 在 compose.yaml 中新增 Zookeeper 和 Nacos 容器配置用于服务注册发现
    - 更新 RocketMQ 的服务地址配置从 192.168.2.74 到 192.168.5.22
    - 为新服务配置 Dubbo 框架集成 Zookeeper 和 Nacos 注册中心
    - 移除公共配置文件中的应用名称设置
    - 添加通用的 IUserService 接口定义
    - 将消息生产和消费相关的类迁移到对应的服务模块中

---
+++ order-service/src/test/java/org/lee/rocket/train/MessageTest.java
@@ -0,0 +1,34 @@
+package org.lee.rocket.train;
+
+import jakarta.annotation.Resource;
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
+import org.junit.jupiter.api.Test;
+import org.lee.rocket.train.common.constant.RocketMQConstants;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @ClassName MessageTest
+ * @Description
+ * @Author lihongliang
+ * @Date 2026/5/31 22:21
+ * @Version 1.0
+ */
+@SpringBootTest
+public class MessageTest {
+
+    @Resource
+    private RocketMQTemplate rocketMQTemplate;
+
+    @Test
+    public void testSendMessage() {
+        // 注意：此测试需要连接到配置的 RocketMQ NameServer (192.168.2.74:9876)
+        // 如果无法连接，请确保：
+        // 1. 网络可达
+        // 2. RocketMQ 服务正在运行
+        // 3. 防火墙允许访问
+        
+//        System.out.println("当前配置的NameServer地址：" + environment.getProperty("rocketmq.name-server"));
+
+        rocketMQTemplate.convertAndSend(RocketMQConstants.DEFAULT_TOPIC, "Hello, RocketMQ!");
+    }
+}
